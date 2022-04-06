fec23de824709431e709e2096bcad6dae5a48fb3
Kelley M. Hess <hess@astro.rug.nl>
2022-04-06 17:48:44 +0200

    Lots of fixes for mosaic option :/

--- src/checkmasks.py
+++ src/checkmasks.py
@@ -20,6 +20,10 @@ def parse_args():
     parser = ArgumentParser(description="Make plots of sourcefinding.py results.",
                             formatter_class=RawTextHelpFormatter)
 
+    parser.add_argument('-l', '--loc', default='output/',
+                        help='Specify the input working directory relative to where you\'re running.'
+                             ' Important for mosaic. (default: %(default)s).')
+
     parser.add_argument('-t', '--taskid', default='190915041',
                         help='Specify the input taskid (default: %(default)s).')
 
@@ -31,16 +35,20 @@ def parse_args():
                         help="Only controls output name of png!  For book keeping purposes.",
                         action='store_true')
 
+    parser.add_argument('-m', "--mosaic",
+                        help="Flag to indicate if operating on a mosaic instead of individual beams.",
+                        action='store_true')
+
     # Parse the arguments above
     args = parser.parse_args()
     return args
 
 
 # If running by itself use: python -m src/checkmasks -t 191004041 -b 14
-def main(taskid, beam=[40], nospline=False, mosaic=False):
+def main(loc, taskid, beam=[40], nospline=False, mosaic=False):
 
     cubes = [1, 2, 3]  # Most sources in 2; nearest galaxies in 3.
-    max_cat_len = 25
+    max_cat_len = 35
 
     HI_restfreq = 1420405751.77 * u.Hz
     optical_HI = u.doppler_optical(HI_restfreq)
@@ -49,21 +57,19 @@ def main(taskid, beam=[40], nospline=False, mosaic=False):
     plt.close('all')
     for b in beam:
         # Define some file names and work space:
-        # loc = taskid + '/B0' + str(b).zfill(2) + '/'              DELETE THIS WHEN NECESSARY *******************************
-        # if mosaic:                                              DELETE THIS WHEN NECESSARY *******************************
-        loc = taskid + '/'
         source_per_beam = 0
         results = glob(loc + 'HI_B0{:02}_cube*sofia_mask-2d.fits'.format(b))
+        if mosaic:
+            results = glob(loc + taskid + '_HIcube*_image_sofia_mask-2d.fits')
         # Output where the program is looking exactly
         print(loc)
-        print(results)
 
         if len(results) > 0:
             print('[CHECKMASKS] Making summary figures/spectra for beam {:02}'.format(b))
             header = fits.getheader(results[0])
             wcs = WCS(header)
             if mosaic:
-                fig_im, ax_im = plt.subplots(1, 1, figsize=(15, 12), subplot_kw={'projection': wcs})
+                fig_im, ax_im = plt.subplots(1, 1, figsize=(15, 12), subplot_kw={'projection': wcs}, squeeze=False)
             else:
                 fig_im, ax_im = plt.subplots(1, 3, figsize=(15, 5), subplot_kw={'projection': wcs})
 
@@ -82,33 +88,35 @@ def main(taskid, beam=[40], nospline=False, mosaic=False):
             for c in cubes:
                 cube_name = 'HI_B0' + str(b).zfill(2) + '_cube' + str(c) + '_image'
                 if mosaic:
-                    cube_name = 'HI_' + taskid + '_cube' + str(c) + '_image'
+                    cube_name = taskid + '_HIcube' + str(c) + '_image'
                 # Make robust for saved data:
+                print(loc + cube_name + '_filtered-2d.fits')
                 if not os.path.isfile(loc + cube_name + '_filtered-2d.fits'):
-                    filter2d(taskid, b, c, overwrite=False)
+                    print("\tTry making 2D plot of filtered pixels.")
+                    filter2d(loc, taskid, b, c, mosaic=mosaic, overwrite=False)
 
                 if os.path.isfile(loc + cube_name + '_filtered-2d.fits'):
-                    print("\tPlotting from 2d filtered file")
+                    print("\tPlotting from 2d filtered file.")
                     hdu_filter = fits.open(loc + cube_name + '_filtered-2d.fits')
-                    filter2d = hdu_filter[0].data[:, :]
-                    filter2d[filter2d == 1] = 9.
+                    filter2d_im = hdu_filter[0].data[:, :]
+                    filter2d_im[filter2d_im == 1] = 9.
                     hdu_filter.close()
                 elif os.path.isfile(loc + cube_name + '_filtered.fits'):
-                    print ("\tPlotting from 3d filtered file")
+                    print("\tPlotting from 3d filtered file.")
                     hdu_filter = fits.open(loc + cube_name + '_filtered.fits')
                     # SVC data has the 0th channel blanked; so use the first channel instead.
-                    filter2d = hdu_filter[0].data[1, :, :]
-                    filter2d[np.isnan(filter2d)] = 9.
-                    filter2d[filter2d < 9] = np.nan
+                    filter2d_im = hdu_filter[0].data[1, :, :]
+                    filter2d_im[np.isnan(filter2d_im)] = 9.
+                    filter2d_im[filter2d_im < 9] = np.nan
                     hdu_filter.close()
-                elif os.path.isfile(loc + cube_name + '_filtered_spline.fits'):
-                    print ("\tShould not be here!")
-                    hdu_filter = fits.open(loc + cube_name + '_filtered_spline.fits')
-                    # SVC data has the 0th channel blanked; so use the first channel instead.
-                    filter2d = hdu_filter[0].data[1, :, :]
-                    filter2d[np.isnan(filter2d)] = 9.
-                    filter2d[filter2d < 9] = np.nan
-                    hdu_filter.close()
+                # elif os.path.isfile(loc + cube_name + '_filtered_spline.fits'):
+                #     print("\tShould not be here!")
+                #     hdu_filter = fits.open(loc + cube_name + '_filtered_spline.fits')
+                #     # SVC data has the 0th channel blanked; so use the first channel instead.
+                #     filter2d_im = hdu_filter[0].data[1, :, :]
+                #     filter2d_im[np.isnan(filter2d_im)] = 9.
+                #     filter2d_im[filter2d_im < 9] = np.nan
+                #     hdu_filter.close()
                 else:
                     print("\tNo continuum filtered file for Beam {:02} Cube {}. Check sourcefinding/ALTA?".format(b, c))
                     # As a precaution, if there is no filtered cube, don't plot source masks either.
@@ -126,27 +134,31 @@ def main(taskid, beam=[40], nospline=False, mosaic=False):
                     mask2d = np.asfarray(mask2d)
                     mask2d[mask2d < 1] = np.nan
 
-                    if os.path.isfile(loc + cube_name + '_all_spline.fits'):
-                        hdu_spline = fits.open(loc + cube_name + '_all_spline.fits')
-                    elif os.path.isfile(loc + cube_name + '_filtered_spline.fits'):
-                        hdu_spline = fits.open(loc + cube_name + '_filtered_spline.fits')
-                    cube_frequencies = chan2freq(np.array(range(hdu_spline[0].data.shape[0])), hdu=hdu_spline[0].header)
+                    # if os.path.isfile(loc + cube_name + '_all_spline.fits'):
+                    #     hdu_spline = fits.open(loc + cube_name + '_all_spline.fits')
+                    # elif os.path.isfile(loc + cube_name + '_filtered_spline.fits'):
+                    #     hdu_spline = fits.open(loc + cube_name + '_filtered_spline.fits')
+                    hdu_spline = fits.open(loc + cube_name + '_spline.fits')
+                    cube_frequencies = chan2freq(np.array(range(hdu_spline[0].data.shape[0])), hdu_header=hdu_spline[0].header)
                     optical_velocity = cube_frequencies.to(u.km/u.s, equivalencies=optical_HI)
 
-                    ax_im[c-1].imshow(filter2d, cmap='Greys_r', vmax=10, vmin=8, origin='lower')
-                    ax_im[c-1].imshow(mask2d, cmap='gist_rainbow', origin='lower')
-                    ax_im[c-1].set_title("Beam {:02} Cube {}".format(b, c))
-                    ra = ax_im[c - 1].coords[0]  # Don't understand why this doesn't work: python 2.7 vs 3?
+                    a = c
+                    if mosaic:
+                        a = 1
+                    ax_im[0, a-1].imshow(filter2d_im, cmap='Greys_r', vmax=10, vmin=8, origin='lower')
+                    ax_im[0, a-1].imshow(mask2d, cmap='gist_rainbow', origin='lower')
+                    ax_im[0, a-1].set_title("Beam {:02} Cube {}".format(b, c))
+                    ra = ax_im[0, a - 1].coords[0]  # Don't understand why this doesn't work: python 2.7 vs 3?
                     ra.set_format_unit(u.hour)
                     for s in range(len(cat)):
-                        ax_im[c-1].text(cat['col3'][s] + np.random.uniform(-40, 40),
-                                        cat['col4'][s] + np.random.uniform(-40, 40),
-                                        cat['col2'][s], color='black')
+                        ax_im[0, a-1].text(cat['col3'][s] + np.random.uniform(-40, 40),
+                                           cat['col4'][s] + np.random.uniform(-40, 40),
+                                           cat['col2'][s], color='black')
                         spectrum = np.nansum(hdu_spline[0].data[:, mask2d == cat['col2'][s]], axis=1)
                         maskmin = chan2freq(cat['col10'][s],
-                                            hdu=hdu_spline[0].header).to(u.km/u.s, equivalencies=optical_HI).value
+                                            hdu_header=hdu_spline[0].header).to(u.km/u.s, equivalencies=optical_HI).value
                         maskmax = chan2freq(cat['col11'][s],
-                                            hdu=hdu_spline[0].header).to(u.km/u.s, equivalencies=optical_HI).value
+                                            hdu_header=hdu_spline[0].header).to(u.km/u.s, equivalencies=optical_HI).value
                         ax_spec[previous + s, 0].plot([optical_velocity[-1].value, optical_velocity[0].value],
                                                       [0, 0], '--', color='gray')
                         ax_spec[previous + s, 0].plot(optical_velocity, spectrum, c=colors[c-1])
@@ -173,7 +185,7 @@ def main(taskid, beam=[40], nospline=False, mosaic=False):
 
                 else:
                     print("\tNO sources in Beam {:02} Cube {}".format(b, c))
-                    ax_im[c - 1].imshow(filter2d, cmap='Greys_r', vmax=10, vmin=8, origin='lower')
+                    ax_im[c - 1].imshow(filter2d_im, cmap='Greys_r', vmax=10, vmin=8, origin='lower')
                     ax_im[c - 1].set_title("Beam {:02} Cube {}".format(b, c))
                     ra = ax_im[c - 1].coords[0]  # Don't understand why this doesn't work: python 2.7 vs 3?
                     ra.set_format_unit(u.hour)
@@ -196,10 +208,13 @@ if __name__ == '__main__':
     # Range of beams to work on:
     taskid = arguments.taskid
     mosaic = arguments.mosaic
+    loc = arguments.loc
     if '-' in arguments.beams:
         b_range = arguments.beams.split('-')
         beams = np.array(range(int(b_range[1])-int(b_range[0])+1)) + int(b_range[0])
     else:
         beams = [int(b) for b in arguments.beams.split(',')]
+    if loc[-1] != '/':
+        loc = loc + '/'
 
-    main(taskid, beams)
+    main(loc, taskid, beams, mosaic=mosaic)
