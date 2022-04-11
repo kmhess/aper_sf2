329a424f70fe4ccfc1ed18e36adb280c1fac72fa
Kelley M. Hess <hess@astro.rug.nl>
2022-04-11 15:10:50 +0200

    If mosaic, make series of spectra plots

--- src/checkmasks.py
+++ src/checkmasks.py
@@ -10,6 +10,7 @@ from astropy import units as u
 from astropy.wcs import WCS
 import matplotlib.pyplot as plt
 import numpy as np
+# import time as testtime
 
 from src.filter2d import filter2d
 
@@ -76,16 +77,22 @@ def main(loc, taskid, beam=[40], cubes=[1, 2, 3], nospline=False, mosaic=False):
             else:
                 fig_im, ax_im = plt.subplots(1, 3, figsize=(15, 5), subplot_kw={'projection': wcs})
 
-            for c in cubes:
-                cube_name = 'HI_image_cube' + str(c)
-                if mosaic:
-                    cube_name = taskid + '_HIcube' + str(c) + '_image'
-                if os.path.isfile(loc + cube_name + '_sofiaFS_cat.txt'):
-                    cat = ascii.read(loc + cube_name + '_sofiaFS_cat.txt')
-                    if len(cat) > max_cat_len:
-                        cat = cat[:max_cat_len]
-                    source_per_beam += len(cat)
-            fig_spec, ax_spec = plt.subplots(source_per_beam, 1, figsize=(15, 3*source_per_beam), squeeze=False)
+            # Probably broken for non mosaic
+            # If not mosaic, plot all integrated spectra on a single plot.
+            if not mosaic:
+                for c in cubes:
+                    # cube_name = 'HI_image_cube' + str(c)
+                    cube_name = 'HI_B0' + str(b).zfill(2) + '_cube' + str(c) + '_image'
+                    # if mosaic:
+                    #     cube_name = taskid + '_HIcube' + str(c) + '_image'
+                    if os.path.isfile(loc + cube_name + '_sofiaFS_cat.txt'):
+                        cat = ascii.read(loc + cube_name + '_sofiaFS_cat.txt')
+                        if len(cat) > max_cat_len:
+                            cat = cat[:max_cat_len]
+                        source_per_beam += len(cat)
+                fig_spec, ax_spec = plt.subplots(source_per_beam, 1, figsize=(15, 3*source_per_beam), squeeze=False)
+            else:
+                source_per_beam = 20
 
             previous = 0
             for c in cubes:
@@ -128,9 +135,11 @@ def main(loc, taskid, beam=[40], cubes=[1, 2, 3], nospline=False, mosaic=False):
                 if os.path.isfile(loc + cube_name + '_sofiaFS_cat.txt'):
                     cat = ascii.read(loc + cube_name + '_sofiaFS_cat.txt')
                     print("\tFound {} sources in Beam {:02} Cube {}".format(len(cat), b, c))
-                    if len(cat) > max_cat_len:
+                    if (len(cat) > max_cat_len) and (not mosaic):
                         print("\tMore than {} candidates: seems this cube is crap".format(max_cat_len))
                         cat = cat[:max_cat_len]
+                    else:
+                        print("\t{} candidates found in the mosaic".format(len(cat)))
                     hdu_mask = fits.open(loc + cube_name + '_sofiaFS_mask-2d.fits')
                     mask2d = hdu_mask[0].data[:, :]
 
@@ -154,13 +163,16 @@ def main(loc, taskid, beam=[40], cubes=[1, 2, 3], nospline=False, mosaic=False):
                     ra = ax_im[0, a - 1].coords[0]  # Don't understand why this doesn't work: python 2.7 vs 3?
                     ra.set_format_unit(u.hour)
                     ax_im[0, a-1].tick_params(axis='both', which='major', labelsize=18)
+
                     for s in range(len(cat)):
                         ax_im[0, a-1].text(cat['col3'][s] + np.random.uniform(-40, 40),
                                            cat['col4'][s] + np.random.uniform(-40, 40),
                                            cat['col2'][s], color='black', fontsize=20)
+
                         # Do spectrum sums faster on subcubes:
+                        # tic1 = testtime.perf_counter()
                         subcube = hdu_spline[0].data[:, int(cat['col8'][s]):int(cat['col9'][s])+1,
-                                                        int(cat['col6'][s]):int(cat['col7'][s])+1]
+                                                     int(cat['col6'][s]):int(cat['col7'][s])+1]
                         submask = mask2d[int(cat['col8'][s]):int(cat['col9'][s])+1,
                                          int(cat['col6'][s]):int(cat['col7'][s])+1]
                         # spectrum = np.nansum(hdu_spline[0].data[:, mask2d == cat['col2'][s]], axis=1)
@@ -169,25 +181,49 @@ def main(loc, taskid, beam=[40], cubes=[1, 2, 3], nospline=False, mosaic=False):
                                             hdu_header=hdu_spline[0].header).to(u.km/u.s, equivalencies=optical_HI).value
                         maskmax = chan2freq(cat['col11'][s],
                                             hdu_header=hdu_spline[0].header).to(u.km/u.s, equivalencies=optical_HI).value
-                        ax_spec[previous + s, 0].plot([optical_velocity[-1].value, optical_velocity[0].value],
-                                                      [0, 0], '--', color='gray')
-                        ax_spec[previous + s, 0].plot(optical_velocity, spectrum, c=colors[c-1])
-                        ax_spec[previous + s, 0].plot([maskmin, maskmin], [np.nanmin(spectrum), np.nanmax(spectrum)],
-                                                      ':', color='gray')
-                        ax_spec[previous + s, 0].plot([maskmax, maskmax], [np.nanmin(spectrum), np.nanmax(spectrum)],
-                                                      ':', color='gray')
-                        ax_spec[previous + s, 0].set_title("Beam {:02}, Cube {}, Source {}".format(b, c, cat['col2'][s]),
-                                                           fontsize=20)
-                        ax_spec[previous + s, 0].set_xlim(optical_velocity[-1].value, optical_velocity[0].value)
+                        # toc1 = testtime.perf_counter()
+                        # print(f"Do {s} subcube: {toc1 - tic1:0.4f} seconds")
+
+                        panel = previous + s
+                        # If working with mosaic cube, only plot 20 spectra at a time...
+                        if mosaic:
+                            panel = s % source_per_beam
+                            if panel == 0:
+                                print("[CHECKMASKS] Making inpsection plot {} of {} for spectra.".format(s//source_per_beam+1,
+                                                                             np.int(np.ceil(len(cat)/source_per_beam))))
+                                fig_spec, ax_spec = plt.subplots(source_per_beam, 1, figsize=(15, 3 * source_per_beam),
+                                                                 squeeze=False)
+                        # Plot spectra
+                        # tic1 = testtime.perf_counter()
+                        ax_spec[panel, 0].plot([optical_velocity[-1].value, optical_velocity[0].value], [0, 0], '--',
+                                               color='gray')
+                        ax_spec[panel, 0].plot(optical_velocity, spectrum, c=colors[c-1])
+                        ax_spec[panel, 0].plot([maskmin, maskmin], [np.nanmin(spectrum), np.nanmax(spectrum)], ':',
+                                               color='gray')
+                        ax_spec[panel, 0].plot([maskmax, maskmax], [np.nanmin(spectrum), np.nanmax(spectrum)], ':',
+                                               color='gray')
+                        ax_spec[panel, 0].set_title("Beam {:02}, Cube {}, Source {}".format(b, c, cat['col2'][s]),
+                                                    fontsize=20)
+                        ax_spec[panel, 0].set_xlim(optical_velocity[-1].value, optical_velocity[0].value)
                         if (np.nanmax(spectrum) > 2.) | (np.nanmin(spectrum) < -2.):
-                            ax_spec[previous + s, 0].set_ylim(np.nanmax(spectrum[cat['col10'][s]:cat['col11'][s]]) * -2,
-                                                              np.nanmax(spectrum[cat['col10'][s]:cat['col11'][s]]) * 2)
-                        ax_spec[previous + s, 0].set_ylabel("Integrated Flux")
-                        if previous + s == source_per_beam - 1:
-                            ax_spec[previous + s, 0].set_xlabel("Optical Velocity [km/s]")
-                        if s+1 >= max_cat_len:
-                            ax_spec[previous + s, 0].text(0.5, 0.05, "Too many spectra to plot, check by hand.",
-                                                          ha='center', transform=ax_spec[previous + s, 0].transAxes)
+                            ax_spec[panel, 0].set_ylim(np.nanmax(spectrum[cat['col10'][s]:cat['col11'][s]]) * -2,
+                                                       np.nanmax(spectrum[cat['col10'][s]:cat['col11'][s]]) * 2)
+                        ax_spec[panel, 0].set_ylabel("Integrated Flux")
+                        # toc1 = testtime.perf_counter ()
+                        # print(f"Do {s} plotting: {toc1 - tic1:0.4f} seconds")
+
+                        # Add x-axis label at the end of making spec plots
+                        if mosaic:
+                            if (panel == source_per_beam - 1) | (s == len(cat) - 1):
+                                ax_spec[panel, 0].set_xlabel("Optical Velocity [km/s]")
+                                fig_spec.savefig(loc + 'HI_image_sofia_summary_spec{}of{}_filtspline.png'.format(s//source_per_beam+1,
+                                                 np.int(np.ceil(len(cat)/source_per_beam))), bbox_inches='tight')
+                        else:
+                            if previous + s == source_per_beam - 1:
+                                ax_spec[previous + s, 0].set_xlabel("Optical Velocity [km/s]")
+                            if s+1 >= max_cat_len:
+                                ax_spec[previous + s, 0].text(0.5, 0.05, "Too many spectra to plot, check by hand.",
+                                                              ha='center', transform=ax_spec[previous + s, 0].transAxes)
                     previous += len(cat)
 
                     hdu_mask.close()
@@ -203,10 +239,12 @@ def main(loc, taskid, beam=[40], cubes=[1, 2, 3], nospline=False, mosaic=False):
 
             if nospline:
                 fig_im.savefig(loc + 'HI_image_sofia_summary.png', bbox_inches='tight')
-                fig_spec.savefig(loc + 'HI_image_sofia_summary_spec.png', bbox_inches='tight')
+                if not mosaic:
+                    fig_spec.savefig(loc + 'HI_image_sofia_summary_spec.png', bbox_inches='tight')
             else:
                 fig_im.savefig(loc + 'HI_image_sofia_summary_filtspline.png', bbox_inches='tight')
-                fig_spec.savefig(loc + 'HI_image_sofia_summary_spec_filtspline.png', bbox_inches='tight')
+                if not mosaic:
+                    fig_spec.savefig(loc + 'HI_image_sofia_summary_spec_filtspline.png', bbox_inches='tight')
             plt.close(fig_im)
             plt.close(fig_spec)
         else:
