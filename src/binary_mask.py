cb7ff3f2005b4fd50aa121717aabb56e8cf88d06
Kelley M. Hess <hess@astro.rug.nl>
2022-06-08 13:39:42 +0200

    Change to 2d binary mask & work with overlapping sources

--- src/binary_mask.py
+++ src/binary_mask.py
@@ -8,7 +8,8 @@ import numpy as np
 ###################################################################
 
 # def parse_args():
-parser = ArgumentParser(description="Regrid mosaic mask to an apertif beam. Assumes spectral dimensions are same.",
+parser = ArgumentParser(description="Make a 2d binary mask including just the objects to be cleaned."
+                                    " Useful to detect which cubes should be regridded & cleaned.",
                         formatter_class=RawTextHelpFormatter)
 
 parser.add_argument('-l', '--loc', default='output/',
@@ -16,7 +17,7 @@ parser.add_argument('-l', '--loc', default='output/',
                          ' Important for mosaic. (default: %(default)s).')
 
 parser.add_argument('-t', '--taskid', default='190915041',
-                    help='Specify the input taskid (default: %(default)s).')
+                    help='Specify the input taskid or field (default: %(default)s).')
 
 parser.add_argument('-c', '--cubes', default='1,2,3', required=True,
                     help='Specify the cubes on which to do source finding (default: %(default)s).')
@@ -31,6 +32,8 @@ args = parser.parse_args()
 
 ###################################################################
 
+
+# To do: Instead of supplying sources, allow option to read in the edited catalog
 
 # def binary_mask(taskid, cubes, sources, njobs):
 
@@ -55,39 +58,39 @@ for c in cubes:
 
     mask_file = loc + cube_name + '_sofiaFS_mask.fits'
     mask_file2d = loc + cube_name + '_sofiaFS_mask-2d.fits'
-    bin_mask_file = loc + cube_name + '_sofiaFS_mask_bin.fits'
+    bin_mask2d_file = loc + cube_name + '_sofiaFS_mask-2d_bin.fits'
     print("[BINARY_MASK] Making {} binary mask including requested sources: {}".format(taskid, sources))
 
     # Define what lines of sight need to be kept based on input sources numbers
-    mask2d = fits.getdata(mask_file2d)
+    mask2d_hdu = fits.open(mask_file2d)
+    mask2d = mask2d_hdu[0].data
     xx, yy = range(mask2d.shape[1]), range(mask2d.shape[0])
     x, y = np.meshgrid(xx, yy)
     x, y = x.ravel(), y.ravel()
     mask2d_lin = mask2d.ravel()
-    src2d = np.array([True if m in sources else False for m in mask2d_lin])
+    # Generalize to allow for overlapping sources by keeping all lines of sight with any object
+    # src2d = np.array([True if m in sources else False for m in mask2d_lin])
+    src2d = np.array([True if m > 0 else False for m in mask2d_lin])
     x, y, mask2d_lin = x[src2d], y[src2d], mask2d_lin[src2d]
 
     tic1 = testtime.perf_counter()
     mask_hdu = fits.open(mask_file)
     mask = mask_hdu[0].data
-    bin_mask = np.zeros(mask.shape)
+    bin_mask2d = np.zeros(mask2d.shape)
 
-    # THIS ISN'T GOING TO WORK FOR OVERLAPPING SOURCES!!!
     for s in range(len(x)):
         mask_lin = mask[:, y[s], x[s]]
         bin_mask_lin = np.array([1 if m in sources else 0 for m in mask_lin])
-        bin_mask[:, y[s], x[s]] = bin_mask_lin
-        # print(x[s],y[s])
+        bin_mask2d[y[s], x[s]] = np.nanmax(bin_mask_lin)
 
     toc1 = testtime.perf_counter()
     print(f"Do binary mask: {toc1 - tic1:0.4f} seconds")
-    # return
 
-    print(bin_mask[670:675, 1240, 600])
-    bin_hdu = fits.PrimaryHDU(data=bin_mask, header=mask_hdu[0].header)
-    bin_hdu.writeto(bin_mask_file, overwrite=True)
+    bin_hdu = fits.PrimaryHDU(data=bin_mask2d, header=mask2d_hdu[0].header)
+    bin_hdu.writeto(bin_mask2d_file, overwrite=True)
 
-
+    mask2d_hdu.close()
+    mask_hdu.close()
 
 # if __name__ == '__main__':
 #     arguments = parse_args()
