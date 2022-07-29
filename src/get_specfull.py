eb5226153f293d4edcc9b57b424f6be411d928f9
Kelley M. Hess <hess@astro.rug.nl>
2022-07-29 19:00:24 +0200

    Code to efficiently extract full spectrum from sources

---
+++ src/get_specfull.py
@@ -0,0 +1,92 @@
+import os
+
+from argparse import ArgumentParser, RawTextHelpFormatter
+from astropy.io import fits, ascii
+from astropy.table import Table
+import astropy.units as u
+from astropy.wcs import WCS
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def chan2freq(channels, header):
+    frequencies = (header['CDELT3'] * (channels - (header['CRPIX3'] - 1)) + header['CRVAL3']) * u.Hz
+    return frequencies
+
+###################################################################
+
+parser = ArgumentParser(description="Create new moment maps for (cleaned!) line cubes for a given taskid, beam, cubes",
+                        formatter_class=RawTextHelpFormatter)
+
+parser.add_argument('-t', '--taskid', default='190915041',
+                    help='Specify the input taskid (default: %(default)s).')
+
+parser.add_argument('-c', '--cubes', default='1,2,3',
+                    help='Specify the cubes on which to do source finding (default: %(default)s).')
+
+parser.add_argument('-s', '--sources', default='all',
+                    help='Specify sources to flag if necessary.  Can specify range or list. (default: %(default)s).')
+
+###################################################################
+
+# Parse the arguments above
+args = parser.parse_args()
+
+
+# Range of cubes/sources to work on:
+taskid = args.taskid
+cubes = [int(c) for c in args.cubes.split(',')]
+
+mos_loc = 'mos_' + taskid + '/'
+for c in cubes:
+    filename = taskid + '_HIcube' + str(c) + '_clean_image'
+    mosaic = fits.open(mos_loc + filename + '.fits')
+    mosaic2d = fits.open(mos_loc + filename + '_mask-2d.fits')
+    wcs_mos = WCS(mosaic[0].header).celestial
+    channels = np.asarray(range(mosaic[0].data.shape[0]))
+    frequency = chan2freq(channels, mosaic[0].header)
+    catalog = Table.read(mos_loc + filename + '_cat.xml')
+    if not os.path.isdir(mos_loc + filename + '_figures'):
+        os.system(f'mkdir {mos_loc}{filename}_figures')
+
+    if args.sources == 'all':
+        sources = [str(s + 1) for s in range(len(catalog))]
+    elif '-' in args.sources:
+        src_range = args.sources.split('-')
+        sources = [str(s + int(src_range[0])) for s in range(int(src_range[1]) - int(src_range[0]) + 1)]
+    else:
+        sources = [str(s) for s in args.sources.split(',')]
+
+    for s in sources:
+        outfile = mos_loc + filename + '_figures/' + filename + '_' + str(s) + '_specfull.txt'
+        src_hdu = fits.open(mos_loc + filename + '_cubelets/' + filename + '_' + str(s) + '_mask.fits')
+        mask2d = np.sum(src_hdu[0].data, axis=0)
+        wcs_src = WCS(src_hdu[0].header).celestial
+        src_hdu.close()
+
+        radec1 = wcs_src.pixel_to_world(0, 0)
+        radec2 = wcs_src.pixel_to_world(mask2d.shape[1]-1, mask2d.shape[0]-1)
+        x1, y1 = wcs_mos.world_to_pixel(radec1)
+        x2, y2 = wcs_mos.world_to_pixel(radec2)
+
+        subcube = mosaic[0].data[:, int(np.rint(y1)):int(np.rint(y2))+1, int(np.rint(x1)):int(np.rint(x2))+1]
+        spectrum = np.nansum(subcube[:, mask2d != 0], axis=1)
+        n_pix = 0 * channels + np.sum(mask2d != 0)
+
+        with open('temp.txt', 'w') as f:
+            f.write("# Integrated source spectrum with noise\n")
+            f.write("# Creator: SoFiA-image-pipeline.py\n")  # %s\n#\n" % sofia_version_full)
+            f.write("# \n")
+            f.write("# The source spectrum, with noise, is calculated by integrating over\n")
+            f.write("# the 2D mask of the source in every channel.  This means every row \n")
+            f.write("# has the same number of contributing pixels and the noise is the same\n")
+            f.write("# for every point.\n")
+            f.write("# \n")
+
+            ascii.write([channels, frequency, spectrum, n_pix], 'temp2.txt', format='fixed_width_two_line',
+                        names=['chan', 'freq', 'f_sum', 'n_pix'])
+
+        os.system("cat temp.txt temp2.txt > {}".format(outfile))
+        os.system("rm temp.txt temp2.txt")
+
+    mosaic.close()
