64838c7186917f4eafe44e8bc76cbc0aadc91457
Kelley M. Hess <kmhess.astro@gmail.com>
2024-05-25 20:33:45 +0200

    Clean up

--- compression.py
+++ compression.py
@@ -2,12 +2,10 @@ from glob import glob
 import os
 
 weights_cubes = glob('/mnt/data/mos_*/*weights.fits')
+noise_cubes = glob('/mnt/data/mos_*/*noise.fits')
 mask_cubes = glob('/mnt/data/mos_*/*mask.fits')
 
-print(weights_cubes[:20])
-print(mask_cubes[:20])
-
-for w in weights_cubes[:5]:
+for w in weights_cubes:
     if not os.path.isfile(w + '.fz') & os.path.isfile(w):
         os.system('fpack {}'.format(w))
         print('fpack {}'.format(w))
@@ -15,10 +13,18 @@ for w in weights_cubes[:5]:
         os.system('rm -r {}'.format(w))
         print('rm -r {}'.format(w))
 
-for m in mask_cubes[:5]:
-    if not os.path.isfile(w + '.fz') & os.path.isfile(m):
+for n in noise_cubes:
+    if not os.path.isfile(n + '.fz') & os.path.isfile(n):
+        os.system('fpack {}'.format(n))
+        print('fpack {}'.format(n))
+    if os.path.isfile(n + '.fz') & os.path.isfile(n):
+        os.system('rm -r {}'.format(n))
+        print('rm -r {}'.format(n))
+
+for m in mask_cubes:
+    if not os.path.isfile(m + '.fz') & os.path.isfile(m):
         os.system('fpack {}'.format(m))
         print('fpack {}'.format(m))
     if os.path.isfile(m + '.fz') & os.path.isfile(m):
         os.system('rm -r {}'.format(m))
-        print('rm -r {}'.format(m))        
+        print('rm -r {}'.format(m))
