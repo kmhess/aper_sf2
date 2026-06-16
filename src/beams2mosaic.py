e6972135d868a757d416788f2d7928243c8dc7be
Tehreem Hai <68086632+tehreem-h@users.noreply.github.com>
2026-06-16 11:48:24 -0400

    Tweaks to read txt files if they exist

--- src/beams2mosaic.py
+++ src/beams2mosaic.py
@@ -2,7 +2,7 @@ import os
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 from astropy.io import fits
-
+from astropy.table import Table
 
 ###################################################################
 
@@ -29,35 +29,59 @@ mosaic = args.mosaic_file
 mosaic_hdu = fits.open(mosaic, mode='update')
 
 field = mosaic.split('/')[-1].split('_')[0] 
-end_name = 'clean_image.fits'
-if 'smooth' in mosaic:
-    end_name = 'clean_smooth_image.fits'
 
 print("[BEAMS2MOSAIC] Adding restoring beam properties to mosaic file {}".format(mosaic))
 for b in range(40):
 
     beam_name = str(b).zfill(2)
-    filename = args.directory + '/' + field + '/HI_B0' + beam_name + '_cube' + args.cubes + '_spline_' + end_name
+    
+    #trying .txt files first
+    end_name = '.txt'
+    if 'smooth' in mosaic:
+        end_name = '_smooth.txt'
+    filename = args.directory + '/' + field + '/HI_B0' + beam_name + '_cube' + args.cubes + end_name
     print('\t',filename)
     if os.path.isfile(filename):
-        file = fits.open(filename)
+        file = Table.read(filename, format='ascii')
         try:
             if mosaic_hdu['BEAM{}'.format(beam_name)]:
                 print("[BEAMS2MOSAIC] BEAM"+str(b).zfill(2)+" extension table already exists.")
                 pass
         except KeyError:
             print("[BEAMS2MOSAIC] Adding channel clean beam properties to BEAM"+str(b).zfill(2)+" extension table.")
-            col1 = fits.Column(name='BMAJ', format='1E', unit='deg', array=file[1].data['BMAJ'])
-            col2 = fits.Column(name='BMIN', format='1E', unit='deg', array=file[1].data['BMIN'])
-            col3 = fits.Column(name='BPA', format='1E', unit='deg', array=file[1].data['BPA'])
-            col4 = fits.Column(name='CHAN', format='1J', array=file[1].data['CHAN'])
+            col1 = fits.Column(name='BMAJ', format='1E', unit='deg', array=file['bmaj'])
+            col2 = fits.Column(name='BMIN', format='1E', unit='deg', array=file['bmin'])
+            col3 = fits.Column(name='BPA', format='1E', unit='deg', array=file['bpa'])
+            col4 = fits.Column(name='CHAN', format='1J', array=file['chan'])
             beam_hdu = fits.BinTableHDU.from_columns([col1, col2, col3, col4])
             beam_hdu.name = 'BEAM{}'.format(beam_name)
             beam_hdu.header.comments['NAXIS2'] = 'number of channels'
             mosaic_hdu.append(beam_hdu)
-        file.close()
     else:
-        print("[BEAMS2MOSAIC] Beam"+str(b).zfill(2)+" image does not exist.")
+        end_name = 'clean_image.fits'
+        if 'smooth' in mosaic:
+            end_name = 'clean_smooth_image.fits'
+        filename = args.directory + '/' + field + '/HI_B0' + beam_name + '_cube' + args.cubes + '_spline_' + end_name
+        print('\t',filename)
+        if os.path.isfile(filename):
+            file = fits.open(filename)
+            try:
+                if mosaic_hdu['BEAM{}'.format(beam_name)]:
+                    print("[BEAMS2MOSAIC] BEAM"+str(b).zfill(2)+" extension table already exists.")
+                    pass
+            except KeyError:
+                print("[BEAMS2MOSAIC] Adding channel clean beam properties to BEAM"+str(b).zfill(2)+" extension table.")
+                col1 = fits.Column(name='BMAJ', format='1E', unit='deg', array=file[1].data['BMAJ'])
+                col2 = fits.Column(name='BMIN', format='1E', unit='deg', array=file[1].data['BMIN'])
+                col3 = fits.Column(name='BPA', format='1E', unit='deg', array=file[1].data['BPA'])
+                col4 = fits.Column(name='CHAN', format='1J', array=file[1].data['CHAN'])
+                beam_hdu = fits.BinTableHDU.from_columns([col1, col2, col3, col4])
+                beam_hdu.name = 'BEAM{}'.format(beam_name)
+                beam_hdu.header.comments['NAXIS2'] = 'number of channels'
+                mosaic_hdu.append(beam_hdu)
+            file.close()
+        else:
+            print("[BEAMS2MOSAIC] Beam"+str(b).zfill(2)+" image does not exist.")
 
 mosaic_hdu.flush()
 mosaic_hdu.close()
